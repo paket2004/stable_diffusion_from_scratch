8fe0ace73474ee38380afb6b70cd906e2e9290f7
Ilia Mitrokhin <103316763+paket2004@users.noreply.github.com>
2025-04-18 08:30:46 +0300

    comments added

--- logic_classes/clip.py
+++ logic_classes/clip.py
@@ -5,22 +5,25 @@ from .clip_layer import CLIPLayer
 class CLIP(nn.Module):
     def __init__(self):
         super().__init__()
+        # num of words, embedding vector len, max_context_length
         self.embedding = CLIPEmbedding(49408, 768, 77)
-
+        # clip layers with number of heads for attention, emb. size and number of layers
         self.layers = nn.ModuleList([
             CLIPLayer(12, 768) for i in range(12)
         ])
-
+        
         self.layernorm = nn.LayerNorm(768)
     
     def forward(self, tokens: torch.LongTensor) -> torch.FloatTensor:
+        # 64 bit signed int
         tokens = tokens.type(torch.long)
-        
+
+        # batch, seq_len -> batch, seq_len, dimension
         state = self.embedding(tokens)
 
         # Apply encoder layers similar to the Transformer's encoder.
         for layer in self.layers: 
             state = layer(state)
         output = self.layernorm(state)
-        
+        # batch size, seq_len, dim
         return output
